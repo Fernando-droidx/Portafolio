c58321be00224006f6b663c703fe3d52571cbf19
Luis Fernando Ramírez Rivas <luis.ramirez4415@alumnos.udg.mx>
2024-09-03 14:12:31 -0600

    commit 18

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default function Index(){
                     <div className="w-64 flex justify-center items-center">
                             <div className="mx-auto px-10 py-8">
                             <div className="container mx-auto py-2">
-                                <img src="https://tecdn.b-cdn.net/img/new/avatars/5.webp" className="mb-4 w-32 rounded-lg" alt="Fernando" />
+                                <img src="images/foto.jpeg" className="mb-4 w-32 rounded-lg" alt="Fernando" />
                                 <h5 className="mb-2 text-xl font-medium leading-tight">Luis Fernando Ramirez Rivas</h5>
                                 <p className="text-neutral-500 dark:text-neutral-400">Computer Science Engineer</p>
                             </div>
@@ -37,13 +37,13 @@ export default function Index(){
                             <div className="mx-auto px-9 py-8">
                             <p className="mb-2 text-xl font-medium leading-tight">Mis lenguajes Favoritos?</p>
                             <p className="text-neutral-500 dark:text-neutral-400">
-                                C# y python son de mis favoritos.<br />
+                                C# y Python son de mis favoritos.<br />
                             </p>
                             </div>
                             <div className="mx-auto px-9 py-8">
                         <p className="mb-2 text-xl font-medium leading-tight">Que idiomas se?</p>
                             <p className="text-neutral-500 dark:text-neutral-400">
-                                Hasta el momento hablo solo 2 idiomas, Espanol e Ingles.<br />
+                                Hasta el momento hablo solo 2 idiomas, Español e Ingles.<br />
                             </p>
                             </div>
                         </div>
