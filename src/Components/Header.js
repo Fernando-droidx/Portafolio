48098e058b1052c8813502e23e6f0bceb10f7831
Luis Fernando Ramírez Rivas <luis.ramirez4415@alumnos.udg.mx>
2024-09-10 13:36:26 -0600

    adding new header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,76 +4,46 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="bg-darkGray border-b p-4 text-white dark:border-black">
-      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-        <div className="flex flex-col md:flex-row md:items-center md:justify-between">
-            <div className="flex items-center">
-              <Link href="/">{/* Aquí pondremos el logo */}
-                  <Image src="/images/2.png" className="mr-10 size-20" width={100} height={100} alt="Logo UDG" />
-              </Link>
-                <div>
-                <h1 className="gradient-text text-3xl font-bold sm:text-3xl">
-                    Luis Fernando Ramirez Rivas
-                </h1>
-
+    <header>
+    <nav class="border-gray-200 bg-white px-4 py-2.5 lg:px-6 dark:bg-gray-800">
+        <div class="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between">
+            <a href="https://flowbite.com" class="flex items-center">
+                <Link src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+                <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite</span>
+            </a>
+            <div class="flex items-center lg:order-2">
+                <a href="#" class="mr-2 rounded-lg px-4 py-2 text-sm font-medium text-gray-800 hover:bg-gray-50 focus:outline-none focus:ring-4 focus:ring-gray-300 lg:px-5 lg:py-2.5 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-800">Log in</a>
+                <a href="#" class="bg-primary-700 hover:bg-primary-800 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 mr-2 rounded-lg px-4 py-2 text-sm font-medium text-white focus:outline-none focus:ring-4 lg:px-5 lg:py-2.5">Get started</a>
+                <button data-collapse-toggle="mobile-menu-2" type="button" class="ml-1 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 lg:hidden dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="size-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                    <svg class="hidden size-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </button>
             </div>
-          </div>
-          <nav className="mt-4 flex w-full justify-between md:mt-0 lg:w-auto">
-            <div className="flex items-center lg:hidden">
-              <button
-                className="text-black/50 dark:text-neutral-200"
-                type="button"
-                data-twe-collapse-init
-                data-twe-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="size-7"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </button>
+            <div class="hidden w-full items-center justify-between lg:order-1 lg:flex lg:w-auto" id="mobile-menu-2">
+                <ul class="mt-4 flex flex-col font-medium lg:mt-0 lg:flex-row lg:space-x-8">
+                    <li>
+                        <a href="#" class="bg-primary-700 lg:text-primary-700 block rounded py-2 pl-3 pr-4 text-white lg:bg-transparent lg:p-0 dark:text-white" aria-current="page">Home</a>
+                    </li>
+                    <li>
+                        <a href="#" class="lg:hover:text-primary-700 block border-b border-gray-100 py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-50 lg:border-0 lg:p-0 lg:hover:bg-transparent dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent lg:dark:hover:text-white">Company</a>
+                    </li>
+                    <li>
+                        <a href="#" class="lg:hover:text-primary-700 block border-b border-gray-100 py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-50 lg:border-0 lg:p-0 lg:hover:bg-transparent dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent lg:dark:hover:text-white">Marketplace</a>
+                    </li>
+                    <li>
+                        <a href="#" class="lg:hover:text-primary-700 block border-b border-gray-100 py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-50 lg:border-0 lg:p-0 lg:hover:bg-transparent dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent lg:dark:hover:text-white">Features</a>
+                    </li>
+                    <li>
+                        <a href="#" class="lg:hover:text-primary-700 block border-b border-gray-100 py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-50 lg:border-0 lg:p-0 lg:hover:bg-transparent dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent lg:dark:hover:text-white">Team</a>
+                    </li>
+                    <li>
+                        <a href="#" class="lg:hover:text-primary-700 block border-b border-gray-100 py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-50 lg:border-0 lg:p-0 lg:hover:bg-transparent dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent lg:dark:hover:text-white">Contact</a>
+                    </li>
+                </ul>
             </div>
-            <div className="hidden w-full lg:flex lg:w-auto lg:items-center" id="navbarSupportedContent">
-              <ul className="flex flex-col lg:flex-row lg:gap-4">
-                <li>
-                  <Link
-                    className="hover:text-hover-purple block py-2 text-black/60 transition duration-200 lg:py-0 dark:text-white/60"
-                    href="/"
-                  >
-                    Home
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="hover:text-hover-purple block py-2 text-black/60 transition duration-200 lg:py-0 dark:text-white/60"
-                    href="/projects"
-                  >
-                    Projects
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    className="hover:text-hover-purple block py-2 text-black/60 transition duration-200 lg:py-0 dark:text-white/60"
-                    href="/experience"
-                  >
-                    Work Experience
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
         </div>
-      </div>
-    </header>
+    </nav>
+</header>
   );
 }
