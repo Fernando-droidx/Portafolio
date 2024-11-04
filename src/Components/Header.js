e2321e9f04e7dc01bed322559b4d328caca6016e
Luis Fernando Ramírez Rivas <114302687+Fernando-droidx@users.noreply.github.com>
2024-11-03 22:11:07 -0600

    Adding new nav

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,8 +9,7 @@ export default function Header() {
 
     <nav class="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+        <Link href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
         </Link>
         <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
