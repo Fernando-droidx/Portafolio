79dfed25099c4eddc7928e8cb47bfdecde0ce575
Luis Fernando Ramírez Rivas <114302687+Fernando-droidx@users.noreply.github.com>
2024-11-06 20:21:11 -0600

    JEJEJEJE

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,18 +7,18 @@ export default function Header() {
     <header>
       
 
-    <nav class="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+    <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
             <span className='text-3xl font-bold sm:text-3xl'>Luis Fernando Ramirez Rivas</span>
         </Link>
-        <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
-          <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+        <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+          <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
             <li>
-              <Link href="/experience" class="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Experience</Link>
+              <Link href="/experience" className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Experiencia</Link>
             </li>
             <li>
-              <Link href="/projects" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Proyectos</Link>
+              <Link href="/projects" className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent" aria-current="page">Proyectos</Link>
             </li>
           </ul>
         </div>
--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,7 +1,7 @@
 
 import React from "react"
-import Header from "../Components/Header";
-import Footer from "../Components/Footer";
+import Header from "./Header";
+import Footer from "./Footer";
 
 export default function Layout({ children }) {
     return (
